e01f590716a3a3fbcfd21763ba9086dcffe312c4
gorbiello <gorbielloo@gmail.com>
2026-06-03 03:05:23 +0700

    Update geoapify_places_service.dart

--- lib/data/services/geoapify_places_service.dart
+++ lib/data/services/geoapify_places_service.dart
@@ -6,78 +6,188 @@ import '../../core/constants/api_keys.dart';
 import '../models/culinary_place_model.dart';
 
 class GeoapifyPlacesService {
-final Map<String, List<String>> styleKeywords = const {
-  'Sumatera Utara': [
-    'lapo',
-    'batak',
-    'rumah makan batak',
-    'masakan batak',
-    'saksang',
-    'arsik',
-    'andaliman',
-    'bpk',
-    'makanan medan',
-  ],
-  'Sumatera Barat': [
-    'padang',
-    'rumah makan padang',
-    'masakan padang',
-    'rendang',
-    'minang',
-    'sate padang',
-    'nasi padang',
-  ],
-  'Yogyakarta': [
-    'gudeg',
-    'jogja',
-    'yogyakarta',
-    'sate klatak',
-    'bakpia',
-  ],
-  'Jawa Timur': [
-    'rawon',
-    'soto lamongan',
-    'pecel madiun',
-    'tahu tek',
-    'jawa timur',
-  ],
-  'Bali': [
-    'bali',
-    'balinese food',
-    'betutu',
-    'ayam betutu',
-    'sate lilit',
-    'lawar',
-  ],
-  'Sulawesi Selatan': [
-    'coto',
-    'coto makassar',
-    'konro',
-    'sop konro',
-    'makassar',
-    'pisang ijo',
-  ],
-  'Papua': [
-    'papua',
-    'papeda',
-    'ikan kuah kuning',
-  ],
-  'Palembang': [
-    'pempek',
-    'palembang',
-    'tekwan',
-    'model palembang',
-  ],
-};
+  static const Map<String, List<String>> styleKeywords = {
+    'Aceh': [
+      'mie aceh',
+      'aceh',
+    ],
+    'Sumatera Utara': [
+      'batak',
+      'lapo',
+      'arsik',
+      'saksang',
+    ],
+    'Sumatera Barat': [
+      'padang',
+      'minang',
+      'rendang',
+      'rumah makan padang',
+    ],
+    'Riau': [
+      'melayu',
+      'riau',
+    ],
+    'Kepulauan Riau': [
+      'melayu',
+      'tanjung pinang',
+    ],
+    'Jambi': [
+      'tempoyak',
+      'jambi',
+    ],
+    'Sumatera Selatan': [
+      'pempek',
+      'tekwan',
+      'palembang',
+    ],
+    'Bengkulu': [
+      'pendap',
+      'bengkulu',
+    ],
+    'Lampung': [
+      'seruit',
+      'lampung',
+    ],
+    'Bangka Belitung': [
+      'mie bangka',
+      'bangka',
+    ],
+    'DKI Jakarta': [
+      'betawi',
+      'kerak telor',
+      'soto betawi',
+    ],
+    'Banten': [
+      'rabeg',
+      'banten',
+    ],
+    'Jawa Barat': [
+      'sunda',
+      'sundanese',
+      'nasi timbel',
+      'karedok',
+    ],
+    'Jawa Tengah': [
+      'soto kudus',
+      'lumpia',
+      'jawa tengah',
+    ],
+    'DI Yogyakarta': [
+      'gudeg',
+      'jogja',
+      'yogyakarta',
+      'sate klathak',
+    ],
+    'Jawa Timur': [
+      'rawon',
+      'pecel',
+      'lamongan',
+      'rujak cingur',
+    ],
+    'Bali': [
+      'betutu',
+      'sate lilit',
+      'bali',
+      'lawar',
+    ],
+    'Nusa Tenggara Barat': [
+      'ayam taliwang',
+      'plecing',
+      'lombok',
+    ],
+    'Nusa Tenggara Timur': [
+      'sei sapi',
+      'ntt',
+      'kupang',
+    ],
+    'Kalimantan Barat': [
+      'choi pan',
+      'singkawang',
+    ],
+    'Kalimantan Tengah': [
+      'kalimantan tengah',
+      'dayak',
+    ],
+    'Kalimantan Selatan': [
+      'soto banjar',
+      'banjar',
+      'ketupat kandangan',
+    ],
+    'Kalimantan Timur': [
+      'samarinda',
+      'kalimantan timur',
+    ],
+    'Kalimantan Utara': [
+      'tarakan',
+      'kalimantan utara',
+    ],
+    'Sulawesi Utara': [
+      'manado',
+      'tinutuan',
+      'rica rica',
+    ],
+    'Gorontalo': [
+      'gorontalo',
+      'binthe biluhuta',
+    ],
+    'Sulawesi Tengah': [
+      'kaledo',
+      'palu',
+    ],
+    'Sulawesi Barat': [
+      'mandar',
+      'sulawesi barat',
+    ],
+    'Sulawesi Selatan': [
+      'coto makassar',
+      'konro',
+      'makassar',
+      'coto',
+    ],
+    'Sulawesi Tenggara': [
+      'sinonggi',
+      'kendari',
+    ],
+    'Maluku': [
+      'papeda',
+      'maluku',
+    ],
+    'Maluku Utara': [
+      'ternate',
+      'maluku utara',
+    ],
+    'Papua': [
+      'papeda',
+      'papua',
+    ],
+    'Papua Barat': [
+      'papeda',
+      'manokwari',
+    ],
+    'Papua Tengah': [
+      'papua',
+    ],
+    'Papua Selatan': [
+      'papua',
+    ],
+    'Papua Pegunungan': [
+      'papua',
+    ],
+    'Papua Barat Daya': [
+      'sorong',
+      'papua',
+    ],
+  };
 
   Future<List<CulinaryPlaceModel>> searchRestaurants({
     required String culinaryStyle,
     required double latitude,
     required double longitude,
-    int radiusMeters = 30000,
+    int radiusMeters = 75000,
   }) async {
     final keywords = styleKeywords[culinaryStyle] ?? [culinaryStyle];
-    final results = <String, CulinaryPlaceModel>{};
+
+    final Map<String, CulinaryPlaceModel> results = {};
 
     for (final keyword in keywords.take(4)) {
       final places = await _requestPlaces(
@@ -88,19 +198,6 @@ final Map<String, List<String>> styleKeywords = const {
       );
 
       for (final place in places) {
-        results[place.id] = place;
-      }
-    }
-
-    if (results.isEmpty) {
-      final fallback = await _requestPlaces(
-        keyword: null,
-        latitude: latitude,
-        longitude: longitude,
-        radiusMeters: radiusMeters,
-      );
-
-      for (final place in fallback) {
         results[place.id] = place;
       }
     }
@@ -124,7 +221,7 @@ final Map<String, List<String>> styleKeywords = const {
       'categories': 'catering.restaurant,catering.fast_food,catering.cafe',
       'filter': 'circle:$longitude,$latitude,$radiusMeters',
       'bias': 'proximity:$longitude,$latitude',
-      'limit': '20',
+      'limit': '50',
       'lang': 'id',
       'apiKey': ApiKeys.geoapifyApiKey,
     };
@@ -146,6 +243,7 @@ final Map<String, List<String>> styleKeywords = const {
     }
 
     final data = jsonDecode(response.body) as Map<String, dynamic>;
+
     final features = data['features'] as List<dynamic>? ?? [];
 
     return features.map((item) {
